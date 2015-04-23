afbdb7dc875e61793572c59702a56bcecf892c99
Christian Meier <post@christianmeier.info>
2015-04-23 16:46:03 +0200

    Renewed license notes

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+################################################################################
+#
+#  Copyright (c) HOFFMANN+LIEBENBERG in association with SNTL Publishing, Berlin
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
 import os
 import sys
 from setuptools import setup
@@ -6,7 +25,7 @@ for path in sys.path:
   if path.startswith(sys.prefix) and path.endswith('site-packages'):
     site_packages = path
 
-VERSION = '3.2.0dev6'
+VERSION = '3.2.1dev'
 
 INSTALL_REQUIRES = [
 # 'ZMS3>=3.1.0',
@@ -35,7 +54,7 @@ CLASSIFIERS = [
   'Intended Audience :: Information Technology',
   'Intended Audience :: Telecommunications Industry',
   'Intended Audience :: Financial and Insurance Industry',
-  'License :: OSI Approved :: GNU General Public License (GPL)',
+  'License :: OSI Approved :: GNU Affero General Public License v3',
 ]
 
 setup(
@@ -46,7 +65,7 @@ setup(
   author                = 'HOFFMANN+LIEBENBERG in association with SNTL Publishing, Berlin',
   author_email          = 'zms@sntl-publishing.com',
   url                   = 'http://www.zms-publishing.com',
-  download_url          = 'https://code.zms3.com/formulator',
+  download_url          = 'https://bitbucket.org/zms3/formulator',
   namespace_packages    = ['zms3'],
   packages              = ['zms3.formulator'],
   install_requires      = INSTALL_REQUIRES,
--- zms3/formulator/ZMSFormulator.py
+++ zms3/formulator/ZMSFormulator.py
@@ -1,5 +1,22 @@
-"""zms3.formulator.ZMSFormulator
-"""
+################################################################################
+#
+#  Copyright (c) HOFFMANN+LIEBENBERG in association with SNTL Publishing, Berlin
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
 from Zope2.App.startup import getConfiguration
 from Products.zms import _globals
 import zExceptions
@@ -7,7 +24,6 @@ import time
 import json
 import urllib
 import urllib2
-
 
 class ZMSFormulator:
   
